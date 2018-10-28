e865c05f202642ce07b4490d0c2681c536938a9b
kismat <kismat@localhost.localdomain>
2018-10-28 07:47:45 +0100

    yeah

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ import { Link, Route,location, Switch } from "@hyperapp/router"
 
 import { App } from './views/containers/App'
 
-const About = () => <h2>About</h2>
 
 export const view = ( state, actions ) => 
 
