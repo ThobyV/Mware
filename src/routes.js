b6a96482f03dd1915c2461439533667e02763c09
kismat <kismat@localhost.localdomain>
2018-10-28 07:13:35 +0100

    yeah

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,14 @@ import { Link, Route,location, Switch } from "@hyperapp/router"
 
 import { App } from './views/containers/App'
 
+const About = () => <h2>About</h2>
 
 export const view = ( state, actions ) => 
 
 <div>
 
 <Switch>    
-    <Route path="/Mware/" render={ App } />       
+    <Route path="/" render={ About } />       
   </Switch>
 
 </div>
